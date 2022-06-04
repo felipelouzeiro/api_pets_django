d81bcee4abbcfaef4983c809a45c8ed2dddfe461
felipe <fl.louzeiro@gmail.com>
2022-06-04 10:45:14 -0300

    feat: implementa view pets

--- pet/views.py
+++ pet/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Pet
+from .serializers import PetSerializer
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
 
-def home(request):
-    return render(request, "pet/home.html")
+class PetList(APIView):
+    def get(self, request, format=None):
+        pets = Pet.objects.all()
+        serializer = PetSerializer(pets, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
