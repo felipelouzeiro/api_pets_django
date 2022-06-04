5a5cc8dc487669405e9dc29e0d2335b03f34dd4c
felipe <fl.louzeiro@gmail.com>
2022-06-04 14:23:03 -0300

    feat: instalando django-cors-headers e configurando o cors

--- adote_um_pet/settings.py
+++ adote_um_pet/settings.py
@@ -40,13 +40,16 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "pet.apps.PetConfig",
-    "rest_framework",
     "adocao.apps.AdocaoConfig",
+    "rest_framework",
+    "corsheaders",
+    "",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -128,3 +131,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 APPEND_SLASH = False
+
+CORS_ALLOW_ALL_ORIGINS = True
