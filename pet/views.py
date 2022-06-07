b01856f906406b2c755af7e5009569f2c34b24f3
felipe <fl.louzeiro@gmail.com>
2022-06-07 12:19:24 -0300

    fix: conserta serializer Pet

--- pet/views.py
+++ pet/views.py
@@ -16,16 +16,15 @@ class PetList(APIView):
         serializer = PetSerializer(pets, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
 
-
-def post(self, request, format=None):
-    serializer = PetSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=HTTP_201_CREATED)
-    return Response(
-        {
-            "message": "houveram erros de validação",
-            "errors": serializer._errors,
-        },
-        status=HTTP_400_BAD_REQUEST,
-    )
+    def post(self, request, format=None):
+        serializer = PetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(
+            {
+                "message": "houveram erros de validação",
+                "errors": serializer._errors,
+            },
+            status=HTTP_400_BAD_REQUEST,
+        )
